8cdaa212cdb655106e76eb7a57fb19ebbc3070c1
qthetruth <shiftrams@gmail.com>
2025-07-29 21:28:14 +0300

    added search substring func

--- src/hwmonitor.h
+++ src/hwmonitor.h
@@ -1,3 +1,10 @@
+#ifndef UTILS
+#define UTILS
 char *read_file(char *filename);
 char *findstr(const char *src, const char *srch, const char *delim);
+#endif
+
+#ifndef CPU
+#define CPU
 void getTotalMem();
+#endif
