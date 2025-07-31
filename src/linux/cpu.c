c2ec7fbdd2d8126ceb7e415bee29235a7e6c9191
qthetruth <shiftrams@gmail.com>
2025-07-31 21:40:23 +0300

    updated paths for findstr()

--- src/linux/cpu.c
+++ src/linux/cpu.c
@@ -13,11 +13,12 @@ CPU *getCPUspecs()
     exit(EXIT_FAILURE);
   }
 
-  char *cpu_info = read_file("/proc/cpuinfo");
+  char regex[] = {'\t'};
+  char *cpu_info = read_file("/proc/cpuinfo", regex);
 
-  cpu->total_cores = atoi(findstr(cpu_info, "cpu cores", "\n"));
-  cpu->total_threads = atoi(findstr(cpu_info, "siblings", "\n"));
-  cpu->model_name = findstr(cpu_info, "model name", "\n");
+  cpu->total_cores = atoi(findstr(cpu_info, "cpu cores: ", "\n"));
+  cpu->total_threads = atoi(findstr(cpu_info, "siblings: ", "\n"));
+  cpu->model_name = findstr(cpu_info, "model name: ", "\n");
 
   free(cpu_info);
   return cpu;
