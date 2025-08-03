ba697d2b58f8f8a183b50e5a6ff1782f4a76e4b9
qthetruth <shiftrams@gmail.com>
2025-08-03 13:30:30 +0300

    Implemented trim() for deleting whitespaces

--- include/utils.h
+++ include/utils.h
@@ -2,5 +2,6 @@
 #define UTILS_H
 char *read_file(char *filename, char *regex);
 char *findstr(const char *src, const char *srch, const char *delim);
+char *trim(char *str);
 int isregex(const char ch, char *regex, int size);
 #endif
--- src/utils.c
+++ src/utils.c
@@ -1,11 +1,47 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
+
+char *trim(char *str)
+{
+  size_t len = 0;
+  char *frontp = str;
+  char *endp = NULL; 
+
+  if ( str == NULL ) { return NULL; }
+  if ( str[0] == '\0' ) { return str; }
+  
+  len = strlen(str);
+  endp = str + len;
+
+  while( isspace((unsigned char) *frontp) ) { ++frontp; }
+  if (endp != frontp)
+  {
+    while( isspace((unsigned char) *(--endp)) && endp != frontp ) {}
+  }
+    
+  if(frontp != str && endp == frontp )
+  {
+    // Empty string
+    *(isspace((unsigned char) *endp) ? str : (endp + 1)) = '\0';
+  }
+  
+  else if ( str + len - 1 != endp ) *(endp + 1) = '\0';
+
+  endp = str;
+  if( frontp != str )
+  {
+    while( *frontp ) { *endp++ = *frontp++; } *endp = '\0';
+  }
+    
+  return str;
+}
 
 char *findstr(const char *src, const char *srch, const char *delim) {
   char *str = strstr(src, srch);
   if (str == NULL) {
-    fprintf(stderr, "Substring not found.");
+    fprintf(stderr, "Substring '%s' not found.\n", srch);
     return NULL;
   }
   str += strlen(srch);
