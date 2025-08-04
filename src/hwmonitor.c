cd2dc63b7664fbb7f43facab83fd032d31890b80
qthetruth <shiftrams@gmail.com>
2025-08-04 14:38:10 +0300

    updated

--- src/hwmonitor.c
+++ src/hwmonitor.c
@@ -6,17 +6,30 @@
 int main(int argc, char *argv[]) {
   
   CPU *cpu = getCPUspecs();
-  
-  printf("Total cores: %d\n", cpu->total_cores);
+  if (cpu == NULL) {
+    fprintf(stderr, "Failed to get CPU specs.");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("CPU Vendor ID: %s", cpu->vendor_id);
+  printf("CPU Family: %d\n", cpu->cpu_family);
+  printf("Model: %d\n", cpu->model);
+  printf("Model name: %s", cpu->model_name);
+  printf("Stepping: %d\n", cpu->stepping);
   printf("Total threads: %d\n", cpu->total_threads);
-  printf("Model name: %s\n", cpu->model_name);
-  
+  printf("Total cores: %d\n", cpu->total_cores);
+
   free(cpu);
   
   MEM *mem = getMEMinfo();
+  if (cpu == NULL) {
+    fprintf(stderr, "Failed to get Memory info.");
+    exit(EXIT_FAILURE);
+  }
   
   printf("Total Mem: %d\n", mem->total);
-  
+  printf("Mem Free: %d\n", mem->free);
+
   free(mem);
   
   return 0;
