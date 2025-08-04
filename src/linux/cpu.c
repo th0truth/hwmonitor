ef94478aa32da9050035d173a236b3881d3d58ea
qthetruth <shiftrams@gmail.com>
2025-08-04 14:35:59 +0300

    Added new CPU specifications

--- src/linux/cpu.c
+++ src/linux/cpu.c
@@ -9,19 +9,25 @@ CPU *getCPUspecs()
   CPU *cpu = (CPU*)malloc(sizeof(CPU));
   if (cpu == NULL) {
     fprintf(stderr, "Memory allocation failed.");
-    free(cpu);
-    exit(EXIT_FAILURE);
+    return NULL;
   }
 
   char regex[] = {'\t', ':'};
-  char *cpu_info = read_file("/proc/cpuinfo", regex);
+  char *cpuinfo = read_file("/proc/cpuinfo", regex);
+  if (cpuinfo == NULL) {
+    free(cpu);
+    return NULL;
+  }
 
-  printf("%s", cpu_info);
+  cpu->vendor_id = findstr(cpuinfo, "vendor_id", "\n");
+  cpu->cpu_family = atoi(findstr(cpuinfo, "cpu family", "\n"));
+  cpu->model = atoi(findstr(cpuinfo, "model", "\n"));
+  cpu->model_name = findstr(cpuinfo, "model name", "\n");
+  cpu->stepping = atoi(findstr(cpuinfo, "stepping", "\n"));
+  cpu->total_threads = atoi(findstr(cpuinfo, "siblings", "\n"));
+  cpu->total_cores = atoi(findstr(cpuinfo, "cpu cores", "\n"));
+  cpu->flags = findstr(cpuinfo, "flags", "\n");
 
-  cpu->total_cores = atoi(findstr(cpu_info, "cpu cores", "\n"));
-  cpu->total_threads = atoi(findstr(cpu_info, "siblings", "\n"));
-  cpu->model_name = findstr(cpu_info, "model name ", "\n");
-
-  free(cpu_info);
+  free(cpuinfo);
   return cpu;
 }
