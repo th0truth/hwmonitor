6e21c7058930f3f7e7279526c486d7d76781069b
qthetruth <shiftrams@gmail.com>
2025-08-04 14:36:52 +0300

    Added new Memory specifications

--- src/linux/mem.c
+++ src/linux/mem.c
@@ -15,8 +15,22 @@ MEM *getMEMinfo()
 
   char regex[] = {' ', ':'};
   char *meminfo = read_file("/proc/meminfo", regex);
+  if (meminfo == NULL) {
+    free(mem);
+    return NULL;
+  }
 
-  mem->total = atoi(findstr(meminfo, "MemTotal", "\n"));
+  mem->total = atoi(findstr(meminfo, "MemTotal", "kB"));
+  mem->free = atoi(findstr(meminfo, "MemFree", "kB"));
+  mem->available = atoi(findstr(meminfo, "MemAvailable", "kB"));
+  mem->buffers = atoi(findstr(meminfo, "Buffers", "kB"));
+  mem->cached = atoi(findstr(meminfo, "Cached", "kB"));
+  mem->swap_total = atoi(findstr(meminfo, "SwapTotal", "kB"));
+  mem->swap_free = atoi(findstr(meminfo, "SwapFree", "kB"));
+  mem->Zswap = atoi(findstr(meminfo, "Zswap", "kB"));
+  mem->Zswapped = atoi(findstr(meminfo, "Zswapped", "kB"));
+  mem->dirty = atoi(findstr(meminfo, "Dirty", "kB"));
+  mem->perCpu = atoi(findstr(meminfo, "Percpu", "kB"));
 
   free(meminfo);
   return mem;
