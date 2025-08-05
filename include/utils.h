db543bc6aeea47ca83d64834491e694c692dac25
qthetruth <shiftrams@gmail.com>
2025-08-05 20:35:38 +0300

    rename a read_file() regex to rmch param

--- include/utils.h
+++ include/utils.h
@@ -1,6 +1,6 @@
 #ifndef UTILS_H
 #define UTILS_H
-char *read_file(char *filename, char *regex);
+char *read_file(char *filename, char *rmch);
 char *findstr(const char *src, const char *srch, const char *delim);
 char *trim(char *str);
 int chmatch(const char c, char *chars, int size);
