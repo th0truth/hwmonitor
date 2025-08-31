884fcff33cd21eda10a6a63e59a63ea25900929c
the truth <shiftrams@gmail.com>
2025-08-31 14:24:27 +0300

    feat: print motherboard info

--- include/pprint.h
+++ include/pprint.h
@@ -5,7 +5,7 @@ void print_CPU();
 void print_GPU();
 void print_MEM();
 void print_OS();
-void print_MOBO();
+void print_Motherboard();
 void print_BIOS();
 void print_MAINBOARD();
 void print_BAT();
