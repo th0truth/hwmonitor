7308b9b685749cf212eee982857650e8c3a790b9
th0truth <shiftrams@gmail.com>
2026-02-28 11:41:56 +0200

    feat(util): add format size

---
+++ include/util.h
@@ -0,0 +1,5 @@
+#pragma once
+
+#include "base.h"
+
+void format_size(char* format, uint64_t size, char* buffer, size_t buf_size);
---
+++ src/util.c
@@ -0,0 +1,27 @@
+#include "base.h"
+
+void format_size(char* target_format, uint64_t bytes, char* buffer, size_t buf_size)
+{
+  const char* suffixes[] = {"EiB", "PiB", "TiB", "GiB", "MiB", "KiB", "B"};
+  
+  // Start at EiB (2^60)
+  uint64_t multiplier = 1ULL << 60; 
+  
+  int target_idx = 0;
+  int num_suffixes = sizeof(suffixes) / sizeof(suffixes[0]);
+
+  for (; target_idx < num_suffixes; target_idx++, multiplier >>= 10) {
+    if (strcmp(suffixes[target_idx], target_format) == 0)
+      break;
+  }
+
+  // If the string wasn't found, default to Bytes
+  if (target_idx == num_suffixes) {
+    target_idx = num_suffixes - 1; 
+    multiplier = 1;
+  }
+
+  double final_value = (double)bytes / (double)multiplier;
+
+  snprintf(buffer, buf_size, "%.2f %s", final_value, suffixes[target_idx]);
+}
